70c07d7581d4e17fb9390ec167fd19041978ecfd
FeAchaleen <feachaleen@gmail.com>
2020-10-04 22:17:50 +0900

    finished Encoder

--- cheolchatbot/langmodel/seq2seq.py
+++ cheolchatbot/langmodel/seq2seq.py
@@ -1,15 +1,33 @@
 import tensorflow as tf
 
-class Seq2seqEncoder(tf.keras.Model):
+class Encoder(tf.keras.Model):
     def __init__(self, vocabSize, embeddingDim, encUnits, batchSz):
-        super(Seq2seqEncoder, self).__init__()
-        raise Exception('Not implemented yet.')
+        super(Encoder, self).__init__()
+        
+        # The batch Size (한 묶음 당 row, 즉 문-답 문장쌍 갯수; 보통 Data fetch과정에서 정해놨을 것)
+        self.batchSz = batchSz
+        # The size; the output of one cell of RNN(GRU here)
+        self.encUnits = encUnits
+        
+        # Embedding layer; The first layer
+        self.embedding = tf.keras.layers.Embedding(vocabSize, embeddingDim)
+        # GRU; RNN Layer; The second layer
+        self.gru = tf.keras.layers.GRU(
+            self.encUnits,
+            return_sequences=True,
+            return_state=True,
+            recurrent_initializer='glorot_uniform'
+        )
+
 
     def call(self, x, hidden):
-        raise Exception('Not implemented yet.')
+        x = self.embedding(x)
+        output, state = self.gru(x, initial_state=hidden)
+        return output, state
+
 
     def initializeHiddenState(self):
-        raise Exception('Not implemented yet.')
+        return tf.zeros((self.batchSz, self.encUnits))
 
 
 class BahdanauAttention(tf.keras.layers.Layer):
@@ -20,7 +38,7 @@ class BahdanauAttention(tf.keras.layers.Layer):
         raise Exception('Not implemented yet.')
 
     
-class Seq2seqDecoder(tf.keras.Model):
+class Decoder(tf.keras.Model):
     def __init__(self, vocabSize, embeddingDim, decUnits, batchSz):
         raise Exception('Not implemented yet.')
     
